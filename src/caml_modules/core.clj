e77ca21f3f6e142d65a2d79af201988be1c5c6e3
Yuri S <yuri.st@gmail.com>
2016-08-13 01:09:09 -0700

    ready for testing

--- src/caml_modules/core.clj
+++ src/caml_modules/core.clj
@@ -1,6 +1,106 @@
 (ns caml-modules.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defmacro struct
+  "Creates a struct from a list of directives.
+  Possible directives:
+  :include expr symbols?
+  :open expr symbols?
+  :let symbol value"
+  [& dirs]
+  (let [go (fn [exps locs dirs]
+             (let [[dir & dirs] dirs]
+               (if-not dir
+                 [exps locs]
+
+                 (let [[op & opers] dir
+
+                       [exps locs]
+                       (case op
+                         :let
+                         (let [[sym ops] opers
+                               symops [sym ops]]
+                           [(conj exps symops) (conj locs symops)])
+
+                         (:open :include)
+                         (let [[e & syms] opers
+                               syms (or syms
+                                        (keys (:exports (eval e))))
+                               exp (gensym "exp")
+                               locs (conj locs [exp `(:exports ~e)])
+                               symops (map (fn [sym] [sym `(~exp '~sym)]) syms)
+                               locs (vec (concat locs symops))
+                               exps (if (= op :include)
+                                      (vec (concat exps symops))
+                                      exps)]
+                           [exps locs]))]
+
+                   (recur exps locs dirs)))))
+
+        [exps locs] (go [] [] dirs)
+        qify (fn [xs] (vec (map (fn [[k _]] `['~k ~k]) xs)))
+        qexps (qify exps)
+        qlocs (qify locs)]
+
+    `(let [~@(mapcat identity locs)]
+       {:exports
+        ~(into {} qexps)
+        :locals
+        ~(into {} qlocs)})))
+
+(def sig* vector)
+
+(defmacro sig [& syms]
+  (let [qsyms (map (fn [s] `'~s) syms)]
+    `(sig* ~@qsyms)))
+
+(defn >< [struct sig]
+  (let [s-keyset (into #{} (keys (:exports struct)))
+        missing (remove s-keyset sig)]
+    (if (seq missing)
+      (throw (Exception. (str "Missing exports " (vec missing))))
+      {:locals (:locals struct)
+       :exports (into {}
+                      (for [k sig] [k ((:exports struct) k)]))})))
+
+(defn $* [struct sym]
+  ((:exports struct) sym))
+
+(defmacro $ [struct sym]
+  `($* ~struct '~sym))
+
+(defrecord FrozenFun [icicle])
+
+(defrecord FrozenSym [icicle])
+
+(defn freeze-fun [fsym & args]
+  (->FrozenFun [fsym args]))
+
+(defn freeze-sym [sym]
+  (->FrozenSym sym))
+
+(defmacro reify-funs [& syms]
+  (let [defs (map (fn [sym] `(def ~sym (partial freeze-fun '~sym))) syms)]
+    `(do ~@defs)))
+
+(defmacro reify-syms [& syms]
+  (let [defs (map (fn [sym] `(def ~sym (freeze-sym '~sym))) syms)]
+    `(do ~@defs)))
+
+(defn thaw [struct e]
+  (let [locs (:locals struct)
+        thaw' (partial thaw struct)]
+    (cond
+      (= (type e) FrozenFun)
+      (let [{[fsym args] :icicle} e]
+        (apply (locs fsym) (map thaw' args)))
+
+      (= (type e) FrozenSym)
+      (let [{sym :icicle} e]
+        (locs sym))
+
+      (fn? e)
+      (fn [x] (thaw' (e x)))
+
+      :else e)))
+
+(def run-in thaw)
--- test/caml_modules/core_test.clj
+++ test/caml_modules/core_test.clj
@@ -1,7 +1,66 @@
 (ns caml-modules.core-test
   (:require [clojure.test :refer :all]
-            [caml-modules.core :refer :all]))
+            [caml-modules.core :refer :all]
+            [clojure.core.match :refer [match]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def A (struct
+        (:let x 5)))
+
+(def B (struct
+        (:include A)))
+
+(def C (struct
+        (:let f (fn [x] (inc x)))))
+
+(def D
+  (let [E (struct
+           (:let e1 10))
+        F (struct
+           #_(:include E e1)
+           (:open E e1)
+           (:let f (inc e1)))]
+    F))
+
+(def S
+  (sig s1 s2))
+
+(def T
+  (struct
+   (:let s1 1)
+   (:let s2 2)
+   (:let s3 3)))
+
+#_(>< D S)
+
+(>< T S)
+
+($ D f)
+
+(def Q
+  (struct
+   (:let f1 (fn [x] (inc x)))
+   (:let c 3)))
+
+(reify-funs f1)
+(reify-syms c)
+
+(run-in Q (f1 5))
+(run-in Q (f1 c))
+(run-in Q c)
+
+(def M
+  (struct
+   (:let c1 5)
+   (:let f1 inc)))
+
+(def N
+  (struct
+   (:include M)
+   (:open M)
+   (:let c1 (inc c1))))
+
+(reify-funs f1)
+(reify-syms c1)
+
+(run-in N (f1 c1))
+(run-in M (f1 c1))
