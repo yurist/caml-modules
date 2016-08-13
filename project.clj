9cce1b27ff3a101cd44d05d2824f35ae651dae81
Yuri S <yuri.st@gmail.com>
2016-08-12 22:51:40 -0700

    initial

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject com.steinschreiber/caml-modules "0.0.1-alpha1"
+  :description "OCaml module/functor polymorphism"
+  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
+                 [org.clojure/clojure "1.9.0-alpha10" :classifier "sources"]
+                 [org.clojure/core.match "0.3.0-alpha4"]])
